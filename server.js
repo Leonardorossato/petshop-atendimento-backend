8c7c81b8edeab5a4fd265d4d30ae2307f84cca6b
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 14:43:23 -0300

    import da conexão com mongoose

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const sequelize = require('./config/db')
+const mongoConnection = require('./config/mongoConnection')
 const atendimentoRouter = require('./routes/atendimentoRouter')
 const fornecedorRouter = require('./routes/fornecedorRouter')
 require('dotenv').config()
 const PORT = process.env.PORT
 
-sequelize.sync().then(() => {
-    console.log('Sqlite3 connection established Ok')
-}).catch((error) => {
-    console.error(error)
-})
-
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.mongoConnection = mongoConnection
 
 app.use('/api', atendimentoRouter)
 app.use('/api', fornecedorRouter)
