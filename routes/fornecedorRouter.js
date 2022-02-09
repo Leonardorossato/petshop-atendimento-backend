57571210cfbb470245745e5b9995f2149024491e
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 15:47:29 -0300

    todas as rotas ok

--- routes/fornecedorRouter.js
+++ routes/fornecedorRouter.js
@@ -4,55 +4,52 @@ const Fornecedores = require('../models/Fornecedores')
 
 router.get('/', async(req, res)=>{
     try {
-        const fornecedor = await Fornecedores.findAll()
+        const fornecedor = await Fornecedores.find()
         res.status(200).json(fornecedor)
     } catch (error) {
         res.status(500).json(error)
     }
 });
 
-router.get('/fornecedor/:id', async(req, res)=>{
+router.get('/fornecedores/:id', async(req, res)=>{
     try {
         const fornecedorId = req.params.id
-        const fornecedor = await Fornecedores.findOne({where: {id: fornecedorId}})
+        const fornecedor = await Fornecedores.findById(fornecedorId)
         res.status(200).json(fornecedor)
     } catch (error) {
         res.status(500).json(error)
     }
 });
 
-router.post('/fornecedor', async (req, res) => {
-    try {
-        const fornecedor = await Fornecedores.create(req.body)
+router.post('/fornecedores', async(req, res) => {
+    const fornecedor = {nome, empresa, email, categoria} = req.body
+    const newFornecedor = new Fornecedores({nome, empresa, email, categoria})
+    newFornecedor.save().then(() =>{
         res.status(201).json(fornecedor)
-    } catch (error) {
-        res.status(404).json(error)
+    }).catch((err) =>{
+        res.status(400).json('Error to add a new payment' + err)
+    });
+})
+
+router.put('/fornecedores/:id', async (req, res) => {
+    try {
+        const updatedFornecdor = await Fornecedores.findByIdAndUpdate(req.params.id,{
+            $set: req.body,
+        },{new: true})
+        res.status(200).json(updatedFornecdor)
+    }catch(err) {
+        res.status(500).json(err)
     }
 })
 
-router.put('/fornecedor/:id', async (req, res) => {
+router.delete('/fornecedores/:id', async (req, res) => {
     try {
-        const fornecedorId = req.params.id
-        const fornecedor = await Fornecedores.findOne({where: {id: fornecedorId}})
-        fornecedor.nome = req.body.nome
-        fornecedor.empresa = req.body.empresa
-        fornecedor.email = req.body.email
-        fornecedor.catergoria = req.body.catergoria
-        await fornecedor.save()
-        res.status(200).json(fornecedor)
-    } catch (error) {
-        res.status(500).json(error)
+        const deltedFornecedor = await Fornecedores.findByIdAndRemove(req.params.id)
+        res.status(200).json(deltedFornecedor)
+    }catch (err) {
+        res.status(500).json(err)
     }
 })
 
-router.delete('/fornecedor/:id', async (req, res) => {
-    try {
-        const fornecedorId = req.params.id
-        const fornecedor = await Fornecedores.destroy({where: {id: fornecedorId}})
-        res.status(200).json(fornecedor)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
 
 module.exports = router
