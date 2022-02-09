8a5783dd163e6bebabfb9106f4ceaa76364d3cc9
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 14:43:08 -0300

    renomeado

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize} = require('sequelize')
-
-const sequelize = new Sequelize('petshop-atendimento-db','fornecedores', 'pass',{
-    dialect: 'sqlite',
-    host: './dev.sqlite'
-})
-
-
-module.exports = sequelize
