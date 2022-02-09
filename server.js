7a4bb05fd17d3bf0287e05a03f7eb023cfdc4b8c
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 09:36:43 -0300

    configuração do sqlite

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 app.use(express.json())
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at ${PORT}`)
