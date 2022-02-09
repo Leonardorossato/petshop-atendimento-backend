f3d659a022c20d3e46b1b806321849c11683d032
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 10:06:51 -0300

    fazendo as rotas

--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Atendimentos = require('../models/Atendimentos')
 
+
+router.post('/atendimento', async (req, res) => {
+    try {
+        const atendimento = await Atendimentos.create(req.body)
+        res.status(201).json(atendimento)
+    } catch (error) {
+        
+    }
+})
 
 
 module.exports = router
