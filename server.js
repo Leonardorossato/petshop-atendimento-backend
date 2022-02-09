dc1b429c869e0f2579c55148ad0a84ebadc2b30b
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 09:43:11 -0300

    passando a conexão com sqlite e verificando se esta ok ou não

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const sequelize = require('./config/db')
 require('dotenv').config()
 const PORT = process.env.PORT
+
+sequelize.sync().then(() => {
+    console.log('Sqlite3 connection established Ok')
+}).catch((error) => {
+    console.error(error)
+})
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
