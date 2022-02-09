b56214220faaaac68c5036df7e9b84dc268423b9
Leonardorossato <leonardorossato21@gmail.com>
2022-02-09 09:15:43 -0300

    configuração da porta

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+require('dotenv').config()
+const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(bodyParser.json())
 
 app.listen(PORT, ()=>{
-    
+    console.log(`Server is running at ${PORT}`)
 })
