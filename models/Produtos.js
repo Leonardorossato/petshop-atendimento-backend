4056524737d9c846520c0160320661a7ef45ae30
Leonardorossato <leonardorossato21@gmail.com>
2022-02-10 09:24:33 -0300

    ok

--- models/Produtos.js
+++ models/Produtos.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const produtosSchema = new mongoose.Schema({
     nome: {type: String,required: true, nullable: false, min: 3, max: 100},
     preco:{type: Number, nullable: false, required: true},
-    estoque:{type: Number,nullable: false, required: true, default: 0}
+    estoque:{type: Number,nullable: false, required: true, default: 0},
+    fornecedorId: {type: mongoose.Schema.Types.ObjectId, ref: 'fornecedores'}
 })
 
 const Produtos = mongoose.model('produtos', produtosSchema)
