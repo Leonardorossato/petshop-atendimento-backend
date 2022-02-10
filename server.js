c217bf381a8af7f34740d94bed0d7ad424b34e08
Leonardorossato <leonardorossato21@gmail.com>
2022-02-10 10:04:18 -0300

    import e uso da rota dos clientes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const mongoConnection = require('./config/mongoConnection')
 const atendimentoRouter = require('./routes/atendimentoRouter')
 const fornecedorRouter = require('./routes/fornecedorRouter')
+const clienteRouter = require('./routes/clienteRouter')
 require('dotenv').config()
 const PORT = process.env.PORT
 
@@ -14,6 +15,7 @@ app.mongoConnection = mongoConnection
 
 app.use('/api', atendimentoRouter)
 app.use('/api/fornecedor', fornecedorRouter)
+app.use('/api/cliente', clienteRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at ${PORT}`)
