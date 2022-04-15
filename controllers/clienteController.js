0dfef1ca40f945195b27dc3713bbebb67e900e38
Leonardorossato <leonardorossato21@gmail.com>
2022-04-15 10:47:17 -0300

    metodo do controller de cliente feito

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -9,6 +9,42 @@ class ClienteController{
             res.status(500).json(error)
         }
     }
+    static getClientById = async(req, res) => {
+        try {
+            const clienteId = req.params.id
+            const cliente = await Cliente.findById(clienteId)
+            res.status(200).json(cliente)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+    static createClient = async(req, res) => {
+        const cliente = {nome, email, endereco, telefone} = req.body
+        const newCliente = new Cliente({nome, email, endereco, telefone})
+        newCliente.save().then(() =>{
+            res.status(201).json(cliente)
+        }).catch((err) =>{
+            res.status(400).json('Error to add a new cliente' + err)
+        });
+    }
+    static updateCliente = async(req, res) => {
+        try {
+            const updateCliente = await Cliente.findByIdAndUpdate(req.params.id,{
+                $set: req.body,
+            },{new: true})
+            res.status(200).json(updateCliente)
+        }catch(err) {
+            res.status(500).json(err)
+        }
+    }
+    static deletedCliente = async(req, res) => {
+        try {
+            const deletedCliente = await Cliente.findByIdAndRemove(req.params.id)
+            res.status(200).json(deletedCliente)
+        }catch (err) {
+            res.status(500).json(err)
+        }
+    }
 }
 
 module.exports = ClienteController
