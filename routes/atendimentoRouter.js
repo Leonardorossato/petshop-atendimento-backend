8b1f2ad6e1566ca34a17e4ca3a2bd49714c20c1e
Leonardorossato <leonardorossato21@gmail.com>
2022-04-15 10:41:17 -0300

    router dos atendimentos feito

--- routes/atendimentoRouter.js
+++ routes/atendimentoRouter.js
@@ -1,55 +1,12 @@
 const express = require('express')
+const AtendimentoController = require('../controllers/atendimentoController')
 const router = express.Router()
-const Atendimento = require('../models/Atendimentos')
 
-router.get('/', async(req, res)=>{
-    try {
-        const atendimento = await Atendimento.find()
-        res.status(200).json(atendimento)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-});
-
-router.get('/atendimento/:id', async(req, res)=>{
-    try {
-        const atendimentoId = req.params.id
-        const atendimento = await Atendimento.findById(atendimentoId)
-        res.status(200).json(atendimento)
-    } catch (error) {
-        res.status(500).json(error)
-    }
-});
-
-router.post('/atendimento', async (req, res) => {
-    const atendimento = { pet, servico, status, data, observacoes} = req.body
-    const newAtendimento = new Atendimento({pet,servico,status,data,observacoes})
-    newAtendimento.save().then(() =>{
-        res.status(201).json(atendimento)
-    }).catch((err) =>{
-        res.status(400).json('Error to add a new atendimento' + err)
-    });
-})
-
-router.put('/atendimento/:id', async (req, res) => {
-    try {
-        const atendimentoUpdated = await Atendimento.findByIdAndUpdate(req.params.id,{
-            $set: req.body,
-        },{new: true})
-        res.status(200).json(atendimentoUpdated)
-    }catch(err) {
-        res.status(500).json(err)
-    }
-})
-
-router.delete('/atendimento/:id', async (req, res) => {
-    try {
-        const deltedAtendimento = await Atendimento.findByIdAndRemove(req.params.id)
-        res.status(200).json(deltedAtendimento)
-    }catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.get('/', AtendimentoController.getAllAtendimentos)
+router.get('/atendimento/:id', AtendimentoController.getAtendimentosById)
+router.post('/', AtendimentoController.createAtendimento)
+router.put('/atendimento/:id', AtendimentoController.updateAtendimento)
+router.delete('/atendimento/:id', AtendimentoController.deltedAtendimento)
 
 
 module.exports = router
